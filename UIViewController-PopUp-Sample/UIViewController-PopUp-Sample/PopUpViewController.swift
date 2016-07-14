0bcb6c9af4d2e3908780dd30778e5466f7365036
litt1e-p <free9fw@gmail.com>
2016-07-14 15:25:28 +0800

    add sample project

---
+++ UIViewController-PopUp-Sample/UIViewController-PopUp-Sample/PopUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PopUpViewController.swift
+//  UIViewController-PopUp-Sample
+//
+//  Created by litt1e-p on 16/7/14.
+//  Copyright © 2016年 litt1e-p. All rights reserved.
+//
+
+import UIKit
+
+class PopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UIViewController-PopUp-Sample/UIViewController-PopUp-Sample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UIViewController-PopUp-Sample
+//
+//  Created by litt1e-p on 16/7/14.
+//  Copyright © 2016年 litt1e-p. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBAction func popUpBtnEvent(sender: AnyObject) {
+        let s                   = UIScreen.mainScreen().bounds.size
+        let pv                  = PopUpViewController()
+        pv.view.frame           = CGRectMake(0, 0, s.width - 2 * 40, s.height * 0.6)
+        pv.view.backgroundColor = .whiteColor()
+        popUpEffectType         = .ZoomIn //.ZoomOut/.FlipUp/.FlipDown
+        presentPopUpViewController(pv)
+    }
+}
+
